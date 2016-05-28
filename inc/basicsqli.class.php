6dec13cdacf91a1bae676a6f40669f57f6f67233
Sérgio Soares <serguiomicaelo@gmail.com>
2016-05-28 10:50:51 +0100

    Add files via upload

--- inc/basicsqli.class.php
+++ inc/basicsqli.class.php
@@ -158,14 +158,8 @@ class BasicSQLi {
         		 */
         		public function fetch()
         		{
-        		if ( $row=mysqli_fetch_array($this->query, MYSQLI_ASSOC) ) {
-        				return $row;
-        			} else if ( $this->size() > 0 ) {
-        				mysqli_data_seek($this->query,0);
-        				return false;
-        			} else {
-        				return false;
-        			}
+        			$row=mysqli_fetch_array($this->query, MYSQLI_ASSOC);
+        			return $row;
         		}
         		
         		/**
